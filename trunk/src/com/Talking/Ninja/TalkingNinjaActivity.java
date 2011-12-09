e0b5c12f8089378ea5f0c267467a9f20f73fe945
johnatkinson23@gmail.com <johnatkinson23@gmail.com@59e270ed-5f05-f9a4-25e3-2c07456139e2>
2011-12-09 07:12:35 +0000

    ken stance to ken roundhouse animation

--- trunk/src/com/Talking/Ninja/TalkingNinjaActivity.java
+++ trunk/src/com/Talking/Ninja/TalkingNinjaActivity.java
@@ -1,25 +1,86 @@
 package com.Talking.Ninja;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class TalkingNinjaActivity extends Activity {
     /** Called when the activity is first created. */
 
    ImageView animation;
+   Button button;
+   //TextView myLabel;
+   View touchView;
+   Animation anim;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        animation = (ImageView)findViewById(R.id.imageAnimation);
-        animation.setBackgroundResource(R.drawable.animation);		// the frame-by-frame animation defined as a xml file within the drawable folder
+        animation = (ImageView)findViewById(R.id.kenstance);
+        animation.setBackgroundResource(R.drawable.ken_stance_animation);		// the frame-by-frame animation defined as a xml file within the drawable folder
+        button = (Button) findViewById(R.id.button_id);
+      
+        animation.setOnTouchListener(new ImageView.OnTouchListener(){
+        	public boolean onTouch(View v, MotionEvent event) {
+        		float xPos = event.getX();
+        		float yPos = event.getY();
+                if ((xPos > 42) && (xPos < 115)&& (yPos > 51) && (yPos < 59)) {
+                	Log.d("ninja", "xPos = " + xPos);
+            		Log.d("ninja", "yPos = " + yPos);
+            		kick(v);
+                }
+                return true;
+        	}
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	kick(v);
+            	Handler mHandler = new Handler();
+            	mHandler.postDelayed(resumeStance, 1050);
+            }
+        });
+    
     }
     
+    public void animation1(float xpos, float ypos, View v){
+    	if ((xpos > 42) && (xpos < 115)&& (ypos > 51) && (ypos < 59)) {
+        	Log.d("ninja", "xPos = " + xpos);
+    		Log.d("ninja", "yPos = " + ypos);
+        	kick(v);
+        }
+    	
+    	
+    }
+    
+    public void kick(View view) {
+    	
+    	animation.setBackgroundResource(R.drawable.ken_rndhouse_animation);
+    	AnimationDrawable frameAnimation = (AnimationDrawable)animation.getBackground();
+    	frameAnimation.start();
+    }
+    
+    private Runnable resumeStance = new Runnable() {
+        public void run() {
+        	animation.setBackgroundResource(R.drawable.ken_stance_animation);
+        	AnimationDrawable frameAnimation = (AnimationDrawable)animation.getBackground();
+        	frameAnimation.start();
+        }
+     };
     @Override
     /**
      * This method is called whenever the Activity becomes visible or invisible to the user.
@@ -35,8 +96,10 @@ public class TalkingNinjaActivity extends Activity {
 			frameAnimation.stop();
 		}
 	}
+    
     public void onTouch (MotionEvent e) {
     	super.onTouchEvent(e);	
+    	
     }
 }
 
